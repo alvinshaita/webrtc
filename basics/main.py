9f2967b8b4006217a801644348de7bb74956be7a
alvinshaita <alvinshaita@gmail.com>
2022-06-28 19:55:24 +0300

    move server to main.py

---
+++ basics/main.py
@@ -0,0 +1,22 @@
+import os
+
+from aiohttp import web
+
+ROOT = os.path.dirname(__file__)
+
+app = web.Application()
+
+app.router.add_get("/", lambda response: web.Response(
+		content_type="text/html",
+		text=open(os.path.join(ROOT, "index.html"), "r").read()
+	)
+)
+
+app.router.add_get("/js/main.js", lambda response: web.Response(
+		content_type="application/javascript",
+		text=open(os.path.join(ROOT, "js/main.js"), "r").read()
+	)
+)
+
+app.router.add_static("/", "..")
+web.run_app(app, host="0.0.0.0", port=8000)
